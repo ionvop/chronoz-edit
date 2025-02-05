23470c3ae0224b992afadcefa150226ffe3fdfd2
ionvop <ionvop@gmail.com>
2025-02-05 16:13:28 +0800

    Delete getDownload.php

--- getDownload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header("Access-Control-Allow-Origin: *");
-
-echo json_encode([
-    "name" => file_get_contents("name.txt"),
-    "song" => file_get_contents("song.txt"),
-    "chart" => file_get_contents("chart.txt")
-]);
